b3b652c313edc9afe88efc2b30d13a08dee28c04
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-09-06 16:05:44 +0700

    e2e scenario: ut002 - ut005

--- test/e2e/scenario.py
+++ test/e2e/scenario.py
@@ -4,7 +4,7 @@ test_cases = [
         "name": """
         [Given] a key is available in "destination"
         [When] a GET request for the key is received
-        [Then] GET and return the key value from "destination
+        [Then] GET and return the key value from "destination"
         """,
         "test": {
             # "given_config": {"delete_on_get": "true", "delete_on_set": "true"},
@@ -12,5 +12,57 @@ test_cases = [
             "when_req_then_resp": [{"req": {"get": ("foo")}, "resp": b"bar"}],
             "then_data": {"src": [], "dst": [{"set": ("foo", "bar")}]},
         },
-    }
+    },
+    {
+        "id": "ut002",
+        "name": """
+        [Given] a key is not available in "destination"
+            [And] "destination" return non-nil error
+        [When] a GET request for the key is received
+        [Then] return the error from "destination"
+        """,
+        "test": {
+            "given_data": {"src": [], "dst": []},
+            "when_req_then_resp": [{"req": {"get": ("foo")}, "resp": None}],
+            "then_data": {"src": [], "dst": []},
+        },
+    },
+    {
+        "id": "ut004",
+        "name": """
+        [Given] a key is not available in "destination"
+            [And] the key is available in "source"
+            [And] deleteOnGet set to false
+        [When] a GET request for the key is received
+        [Then] GET and return the key value from "source"
+            [And] SET the value with the key to "destination"
+        """,
+        "test": {
+            "given_config": {"delete_on_get": "false"},
+            "given_data": {"src": [{"set": ("foo", "bar")}], "dst": []},
+            "when_req_then_resp": [{"req": {"get": ("foo")}, "resp": b"bar"}],
+            "then_data": {
+                "src": [{"set": ("foo", "bar")}],
+                "dst": [{"set": ("foo", "bar")}],
+            },
+        },
+    },
+    {
+        "id": "ut005",
+        "name": """
+        [Given] a key is not available in "destination"
+            [And] the key is available in "source"
+            [And] deleteOnGet set to true
+        [When] a GET request for the key is received
+        [Then] GET and return the key value from "source"
+            [And] SET the value with the key to "destination"
+            [And] DELETE the key from "source"
+        """,
+        "test": {
+            "given_config": {"delete_on_get": "true"},
+            "given_data": {"src": [{"set": ("foo", "bar")}], "dst": []},
+            "when_req_then_resp": [{"req": {"get": ("foo")}, "resp": b"bar"}],
+            "then_data": {"src": [], "dst": [{"set": ("foo", "bar")}]},
+        },
+    },
 ]
