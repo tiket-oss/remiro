3c650341f6829b2d0ccfa6d69fea62d909311f40
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-09-06 14:26:27 +0700

    clean-docker.sh: clean volumes and networks

--- test/e2e/clean-docker.sh
+++ test/e2e/clean-docker.sh
@@ -3,6 +3,8 @@
 docker container prune
 docker rm $(docker container ls -f name="redis" -q) --force
 docker rm $(docker container ls -f name="remiro" -q) --force
+docker network rm $(docker network ls -f name="e2e" -q)
+docker volume rm $(docker volume ls -f name="e2e" -q)
 docker image prune
 # docker rmi $(docker images "remiro*" -q) --force
 # docker rmi $(docker images "redis-rdb-tools*" -q) --force             
