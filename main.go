306fc36da4baa2425622475e60f1f0f928c2fa93
Hawari Rahman <hawari.rahman17@gmail.com>
2019-10-23 13:42:57 +0700

    Tidy up main.go source code

--- main.go
+++ main.go
@@ -19,27 +19,29 @@ func main() {
 	flag.StringVarP(&instruPort, "instru-port", "i", "8888", "configure the port for providing instrumentation")
 	flag.StringVarP(&configPath, "config", "c", "config.toml", "configuration file to use")
 	flag.BoolVarP(&verbose, "verbose", "v", false, "Set remiro to be verbose, logging every events that happened")
-
 	flag.Parse()
 
+	config, _ := readConfig(configPath)
+	redisHandler := handler.NewRedisHandler(config)
+	addr := fmt.Sprintf("%s:%s", host, port)
+	instruAddr := fmt.Sprintf("%s:%s", host, instruPort)
 	if verbose {
 		log.SetLevel(log.TraceLevel)
 	}
-	config, _ := readConfig(configPath)
-	addr := fmt.Sprintf("%s:%s", host, port)
-	redisHandler := handler.NewRedisHandler(config)
-	instruAddr := fmt.Sprintf("%s:%s", host, instruPort)
 
-	instruErr := make(chan error)
-	if err := handler.RunInstrumentation(instruAddr, redisHandler, instruErr); err != nil {
+	fmt.Printf("Preparing instrumentation endpoints...\n")
+	runInstruErr := make(chan error)
+	if err := handler.RunInstrumentation(instruAddr, redisHandler, runInstruErr); err != nil {
 		log.Fatalf("Failed to run instrumentation server: %v", err)
+	} else {
+		go func() { log.Warn(<-runInstruErr) }()
+		fmt.Printf("Instrumentation is available at %s\n", instruAddr)
 	}
 
+	fmt.Printf("Remiro is now running at %s\n", addr)
 	if err := handler.Run(addr, redisHandler); err != nil {
 		log.Fatal(err)
 	}
-
-	log.Warn(<-instruErr)
 }
 
 func readConfig(configPath string) (handler.RedisConfig, error) {
